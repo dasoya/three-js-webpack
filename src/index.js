427bd8d3a32a9d89ce07be441f9abe256462ad59
dasoya <66422476+dasoya@users.noreply.github.com>
2024-06-12 17:39:43 +0900

    geo 추가

--- src/index.js
+++ src/index.js
@@ -9,18 +9,46 @@ if (WEBGL.isWebGLAvailable()) {
     
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,0.1,1000);
 
-    const canvas = document.querySelector('#index');
-
-    const renderer = new THREE.WebGLRenderer({canvas});
+    camera.position.z = 3;
+    const renderer = new THREE.WebGLRenderer({
+      alpha : true,
+      antialias : true
+    });
     renderer.setSize(window.innerWidth,window.innerHeight);
 
-    //document.body.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
 
+    const geometry01 = new THREE.OctahedronGeometry(1,1,1);
+    const material01 = new THREE.MeshStandardMaterial({
+      color:0X999999
+    });
+
+    const obj01 = new THREE.Mesh(geometry01,material01);
+    scene.add(obj01);
+
+    const geometry02 = new THREE.OctahedronGeometry(1,1,1);
+    const material02 = new THREE.MeshStandardMaterial({
+      color:0X999999
+    });
+
+    const obj02 = new THREE.Mesh(geometry02,material02);
+    scene.add(obj02);
+    obj02.position.x = -2;
+    const geometry03 = new THREE.OctahedronGeometry(1,1,1);
+    const material03 = new THREE.MeshStandardMaterial({
+      color:0X999999
+    });
+
+    const obj03 = new THREE.Mesh(geometry03,material03);
+    obj03.position.x = 2;
+    scene.add(obj03);
+    
     function render(time){
 
        time *= 0.001;
       
-      
+       obj01.rotation.x = time;
+       obj01.rotation.y = time;
        renderer.render(scene,camera); 
        requestAnimationFrame(render);
 
@@ -28,6 +56,15 @@ if (WEBGL.isWebGLAvailable()) {
  
       requestAnimationFrame(render);
 
+    
+    function onWindowResize(){
+      camera.aspect = window.innerWidth / window.innerHeight ;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth,window.innerHeight);
+
+    }
+
+    window.addEventListener('resize', onWindowResize);
 
 } else {
   var warning = WEBGL.getWebGLErrorMessage()
